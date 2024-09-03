d5a55eba6836a151dbaeef1015e2d639098da7fa
Aya Ahmed Kenawy <ayahkenawy4490@gmail.com>
2024-09-03 16:14:46 +0300

    CreateTask

---
+++ Freelancers.Api/Validators/Tasks/CreateTaskRequestValidator.cs
@@ -0,0 +1,10 @@
+﻿//using Freelancers.Core.Contracts.Tasks;
+
+
+//namespace Freelancers.Api.Validators.Tasks
+//{
+//    public class CreateTaskRequestValidator : AbstractValidator<CreateTaskRequest>
+//    {
+      
+//    }
+//}
